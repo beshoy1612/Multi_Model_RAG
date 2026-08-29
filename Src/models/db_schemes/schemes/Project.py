f65968ad88c645683b6a07160848d6c5835241b1
beshoy_karam <beshoy1612.karam@gmail.com>
2026-08-29 19:52:40 +0300

    adding project scheme

--- Src/models/db_schemes/schemes/Project.py
+++ Src/models/db_schemes/schemes/Project.py
@@ -1,7 +1,17 @@
 from .multi_rag_base import SQLalchemy_base
-from sqlalchemy import Column,Integer,DateTime
+from sqlalchemy import Column,Integer,DateTime,func
+from sqlalchemy.dialects.postgresql import UUID
+import uuid
+
 class Project(SQLalchemy_base):
     #first thing in database is to define your tables
     __tablename__ = "Projects"
     Projcet_id = Column(Integer,primary_key=True,autoincrement=True)
-        
+    #adding unique value to project_id to add security 
+    # this column doesnt accept null value must has avalue nullable=False
+    Project_uuid = Column(UUID(as_uuid=True),default=uuid.uuid4,unique=True,nullable=False)
+
+    created_at = Column(DateTime(timezone=True),server_default=func.now(),nullable=False)
+    updated_at = Column(DateTime(timezone=True),onupdate=func.now(),nullable=True)
+
+    
